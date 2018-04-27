ff55fb18fd651177f45fff08fca1f0a8467e2c60
Brady <bbbove20@gmail.com>
2018-04-27 06:46:43 -0600

    finishing adding larg-hero content.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-const path = "./gulp-tasks";
+const path = "./gulp-tasks/";
 
 require(path + "styles");
 require(path + "scripts");
-// require(path + "sprite");
+require(path + "sprite");
 require(path + "watch");
 require(path + "modernizr");
 require(path + "build");
